17613421dd44a6533d9fcdef103ea5026610f95e
mohammedrezq <mohammedrezq2000@gmail.com>
2019-05-08 20:56:01 +0200

    git commit wp-content/themes/twentysixteen/template-parts/content.php

---
+++ wp-content/themes/asbab/functions.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * asbab functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package asbab
+ */
+
+if ( ! function_exists( 'asbab_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function asbab_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on asbab, use a find and replace
+		 * to change 'asbab' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'asbab', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'menu-1' => esc_html__( 'Primary', 'asbab' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'asbab_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'asbab_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function asbab_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'asbab_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'asbab_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function asbab_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'asbab' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'asbab' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'asbab_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function asbab_scripts() {
+
+
+    /*Asbab Main Styles*/
+    wp_enqueue_style('asbab-bootstrap', get_template_directory_uri() .'/css/bootstrap.min.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-owlcss', get_template_directory_uri() .'/css/owl.carousel.min.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-owl-theme-css', get_template_directory_uri() .'/css/owl.theme.default.min.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-core-css', get_template_directory_uri() .'/css/core.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-shortcode-css', get_template_directory_uri() .'/css/shortcode/shortcodes.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-responsive-css', get_template_directory_uri() .'/css/responsive.css', array(), '20190508');
+
+    wp_enqueue_style('asbab-custom-css', get_template_directory_uri() .'/css/custom.css', array(), '20190508');
+
+
+    /*Asbab Main scripts*/
+
+    /*modernizr script in header! */
+    wp_enqueue_script( 'asbab_modernizr', get_template_directory_uri() . '/js/vendor/modernizr-3.5.0.min.js', array(), '20190508', false);
+
+    wp_enqueue_script( 'asbab_jquery', get_template_directory_uri() . '/js/vendor/jquery-3.2.1.min.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_pluginsjs', get_template_directory_uri() . '/js/plugins.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_slick', get_template_directory_uri() . '/js/slick.min.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_waypoints', get_template_directory_uri() . '/js/waypoints.min.js', array(), '20190508', true);
+
+    wp_enqueue_script( 'asbab_mainjs', get_template_directory_uri() . '/js/main.js', array(), '20190508', true);
+
+    /*Main Underscore scripts*/
+
+	wp_enqueue_style( 'asbab-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'asbab-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'asbab-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'asbab_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
+/**
+ * Load WooCommerce compatibility file.
+ */
+if ( class_exists( 'WooCommerce' ) ) {
+	require get_template_directory() . '/inc/woocommerce.php';
+}
---
+++ wp-content/themes/gulp-dev/gulpfile.js
@@ -0,0 +1,107 @@
+const themename = 'asbab';
+
+const gulp = require('gulp'),
+// Prepare and optimize code etc
+autoprefixer  = require('autoprefixer'),
+browserSync   = require('browser-sync').create(),
+imagemin      = require('gulp-image'),
+sass          = require('gulp-sass'),
+sourcemaps    = require('gulp-sourcemaps'),
+concat        = require('gulp-concat'),
+rename        = require('gulp-rename'),
+
+root = '../' + themename + '/',
+scss = root + 'sass/',
+bootstrap = scss + 'bootstrap/',
+js = root + 'js/',
+images = root + 'image/',
+imagemini = root + 'img/',
+fonts = root + 'fonts/',
+css = root + 'css/';
+
+gulp.task('images', function() {
+	return gulp.src(images + '/**/*.{jpg,JPG,png}')
+			.pipe(imagemin())
+			.pipe(gulp.dest(imagemini));
+});
+
+gulp.task('bootstrap', function () {
+	return gulp.src(['node_modules/bootstrap/scss/*'])
+	.pipe(gulp.dest(bootstrap));
+});
+
+gulp.task('vendorbs', function () {
+    return gulp.src('node_modules/bootstrap/scss/vendor/*')
+        .pipe(gulp.dest(bootstrap + 'vendor/'));
+});
+
+gulp.task('sass', function () {
+return gulp.src([scss + '{style.scss,rtl.scss}'])
+.pipe(sourcemaps.init())
+.pipe(sass({
+    outputStyle: 'expanded',
+    indentType: 'tab',
+    indentWidth: '1'
+}).on('error', sass.logError))
+.pipe(sourcemaps.write(scss + 'maps'))	
+.pipe(gulp.dest(root))
+.pipe(browserSync.stream());
+
+});
+
+
+// Add all External Css files into css folder
+gulp.task('styles', function () {
+return gulp.src([scss])
+.pipe(sourcemaps.init())
+.pipe(sass({
+outputStyle: 'expanded',
+indentType: 'tab',
+indentWidth: '1'
+}).on('error', sass.logError))
+.pipe(sourcemaps.write(scss + 'maps'))	
+.pipe(gulp.dest(css))
+// .pipe(browserSync.stream());
+
+});
+
+// Move JS files to production/JS
+gulp.task('js', function(){
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js','node_modules/owl.carousel/docs/assets/owlcarousel/owl.carousel.js','node_modules/jquery-circle-progress/dist/circle-progress.js','node_modules/jquery.counterup/jquery.counterup.js','node_modules/wowjs/dist/wow.js','node_modules/aos/dist/aos.js','node_modules/jquery.animate-number/jquery.animateNumber.min.js','node_modules/jquery.easing/jquery.easing.js','node_modules/jquery.stellar/jquery.stellar.js','node_modules/waypoints/lib/jquery.waypoints.js','node_modules/jquery-migrate/dist/jquery-migrate.min.js','node_modules/scrollax/scrollax.js','node_modules/magnific-popup/dist/jquery.magnific-popup.js', js + '*.js'])
+  .pipe(gulp.dest(js))
+  // .pipe(browserSync.stream());
+});
+
+
+
+
+// Move Fonts Folder to production/fonts
+gulp.task('fonts', function(){
+    return gulp.src(['node_modules/font-awesome/fonts/*','node_modules/ionicons/dist/fonts/*','node_modules/icomoon/style.css','node_modules/open-iconic/font/fonts'])
+        .pipe(gulp.dest(fonts));
+});
+
+// Icomoon Directory for the styles
+gulp.task('icomoon', function(){
+    return gulp.src('node_modules/icomoon/fonts/*')
+        .pipe(gulp.dest(fonts + 'fonts/'))
+});
+
+// Watch Sass & server
+gulp.task('serve',['sass','styles','bootstrap','vendorbs','js','images','fonts','icomoon'], function(){
+  browserSync.init({
+			open: 'external',
+			proxy: 'localhost/stories/',
+			port: 9991
+  });
+  gulp.watch(root + '**/*.scss' ,['sass','styles','bootstrap']);
+  gulp.watch(js + '**/*.js',['js']);
+	gulp.watch(images , ['images']);
+	gulp.watch(root + '**/*.php').on('change', browserSync.reload);
+});
+
+
+
+
+
+gulp.task('default',['serve']);
